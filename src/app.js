ceaa4649920ca978cf171e2e13237208ba7626e4
Peter Danis <peter.danis@outlook.com>
2019-11-04 16:16:42 +0100

    Set default error status code

--- src/app.js
+++ src/app.js
@@ -5,12 +5,15 @@ const setupRateLimiter = require("./utils/rateLimiter");
 
 const app = express();
 
+// Setup logging and rate limiter
 setupLog(app);
 setupRateLimiter(app);
 
-// Setup headers and json
+// Disable headers
 app.disable("etag");
 app.disable("x-powered-by");
+
+// Use JSON middleware
 app.use(express.json());
 
 // Processes router handler
@@ -19,14 +22,18 @@ app.use("/processes", processesRouter);
 // 404 handler
 app.use("*", (req, res, next) => {
   res.status(404);
-  next(`Cannot ${req.method} ${req.url}`);
+  next("Not Found");
 });
 
 // Error handler
 // eslint-disable-next-line no-unused-vars
 app.use((err, req, res, next) => {
-  res.status(res.statusCode || err.status || 500);
-  res.json(err);
+  if (res.statusCode >= 400) {
+    res.status(res.statusCode);
+  } else {
+    res.status(500);
+  }
+  res.json({ error: err.message || err });
 });
 
 module.exports = app;
