64da0a4af36ee158073a04b8e54989b2d4b3d700
Peter Danis <peter.danis@outlook.com>
2020-01-14 16:00:23 +0100

    Make the logoff process more universal

--- src/routes/misc.js
+++ src/routes/misc.js
@@ -1,6 +1,15 @@
 const { version } = require("../../package.json");
-const execFile = require("util").promisify(require("child_process").execFile); // eslint-disable-line security/detect-child-process
+const exec = require("util").promisify(require("child_process").exec); // eslint-disable-line security/detect-child-process
 const express = require("express");
+
+// eslint-disable-next-line consistent-return
+const logoff = value => {
+  // eslint-disable-next-line security/detect-unsafe-regex
+  const id = value.match(/((?<id>[1-9][0-9]*)[ ]+)(disc|active)/i);
+  if (id) {
+    return exec(`logoff ${id.groups.id}`);
+  }
+};
 
 const router = express.Router();
 
@@ -10,19 +19,23 @@ router.get("/version", (req, res) => {
 
 router.post("/logoff", async (req, res, next) => {
   try {
-    await execFile("taskkill", ["/IM", "automate.exe", "/F"]);
+    await exec("taskkill /IM automate.exe /F");
   } catch (error) {
     if (
-      !error.message ||
-      !error.message.match(/process "automate.exe" not found/)
+      error.message &&
+      error.message.match(/process "automate.exe" not found/)
     ) {
+      // Ignore "process not running" error, as the automate.exe does not have to run at the moment
+    } else {
       next(error);
       return;
     }
   }
 
   try {
-    await execFile("shutdown", ["/L", "/F"]);
+    // Get all sessions and logoff them
+    const { stdout } = await exec("qwinsta");
+    await Promise.all(stdout.match(/[^\r\n]+/g).map(logoff));
     res.status(202).json({ status: "Logging off" });
   } catch (error) {
     next(error);
