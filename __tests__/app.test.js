e311da05ab64963bbddb2fbc4c44fd5e821bc583
Peter Danis <peter.danis@outlook.com>
2020-01-23 16:14:51 +0100

    Add more tests

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,20 +3,45 @@ const app = require("../src/app");
 const request = require("supertest");
 const { version } = require("../package.json");
 
-const { BP_API_AUTH_PASSWORD, BP_API_AUTH_USERNAME } = process.env;
+const {
+  BP_API_AUTH_PASSWORD: pw,
+  BP_API_AUTH_USERNAME: username,
+} = process.env;
 
 describe("App", () => {
   test("should require authentication", async () => {
     const res = await request(app).get("/version");
     expect(res.status).toBe(401);
   });
-  describe("/version route", () => {
+  test("should return 404 for unknown route", async () => {
+    const res = await request(app)
+      .get("/unknownroute")
+      .auth(username, pw);
+    expect(res.status).toBe(404);
+  });
+  describe("GET /version", () => {
     test("should return version number from package.json", async () => {
       const res = await request(app)
         .get("/version")
-        .auth(BP_API_AUTH_USERNAME, BP_API_AUTH_PASSWORD);
+        .auth(username, pw);
       expect(res.status).toBe(200);
       expect(res.body.version).toBe(version);
+    });
+  });
+  describe("POST /processes", () => {
+    test("should return 400 if process name is missing", async () => {
+      const res = await request(app)
+        .post("/processes")
+        .auth(username, pw);
+      expect(res.status).toBe(400);
+    });
+  });
+  describe("GET /processes/{sessionId}", () => {
+    test("should return 400 if sessionId has incorrect format", async () => {
+      const res = await request(app)
+        .get("/processes/badId")
+        .auth(username, pw);
+      expect(res.status).toBe(400);
     });
   });
 });
