c43e1fcde6c5b80e0abd980d2e0fd18ea94394fd
Peter Danis <peter.danis@outlook.com>
2020-01-29 23:07:22 +0100

    Prepare child_process mocks

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,5 +1,7 @@
 require("dotenv").config();
 const app = require("../src/app");
+// eslint-disable-next-line security/detect-child-process
+const cp = require("child_process");
 const request = require("supertest");
 const { version } = require("../package.json");
 
@@ -7,6 +9,20 @@ const {
   BP_API_AUTH_PASSWORD: pw,
   BP_API_AUTH_USERNAME: username,
 } = process.env;
+
+const execFileMock = (err, input) => (...args) => {
+  args.pop()(err, { stdout: input });
+};
+
+const execMock = (err, input) => (...args) => {
+  args.pop()(err, input);
+};
+
+jest.mock("child_process");
+
+// TODO: create mock implementation helpers, will be called multiple times from tests, mock inputs needs to be different
+cp.exec.mockImplementation(execMock(null, {}));
+cp.execFile.mockImplementation(execFileMock(null, "session:testId"));
 
 describe("App", () => {
   test("should require authentication", async () => {
@@ -19,6 +35,7 @@ describe("App", () => {
       .auth(username, pw);
     expect(res.status).toBe(404);
   });
+
   describe("GET /version", () => {
     test("should return version number from package.json", async () => {
       const res = await request(app)
@@ -28,6 +45,7 @@ describe("App", () => {
       expect(res.body.version).toBe(version);
     });
   });
+
   describe("POST /processes", () => {
     test("should return 400 if process name is missing", async () => {
       const res = await request(app)
@@ -35,13 +53,35 @@ describe("App", () => {
         .auth(username, pw);
       expect(res.status).toBe(400);
     });
+    test("should return 400 if inputs are wrong", async () => {
+      // TODO: finish test
+      const res = await request(app)
+        .post("/processes")
+        .send({
+          process: "Test",
+        })
+        .auth(username, pw);
+      expect(res.body).toBe();
+      expect(res.status).toBe(400);
+    });
   });
+
   describe("GET /processes/{sessionId}", () => {
     test("should return 400 if sessionId has incorrect format", async () => {
       const res = await request(app)
         .get("/processes/badId")
         .auth(username, pw);
       expect(res.status).toBe(400);
+    });
+  });
+
+  describe("POST /reset", () => {
+    // TODO: finish test
+    test("TODO", async () => {
+      const res = await request(app)
+        .post("/reset")
+        .auth(username, pw);
+      expect(res.body).toBe();
     });
   });
 });
