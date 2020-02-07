94c7066cac5f5d287fd47257082662dad5ae189d
Peter Danis <peter.danis@outlook.com>
2020-02-07 13:30:13 +0100

    Add reset / kill config

--- src/routes/misc.js
+++ src/routes/misc.js
@@ -2,12 +2,26 @@ const { version } = require("../../package.json");
 const exec = require("util").promisify(require("child_process").exec); // eslint-disable-line security/detect-child-process
 const express = require("express");
 
+const { BP_API_FULL_RESET } = process.env;
+
 // eslint-disable-next-line consistent-return
 const logoff = value => {
   // eslint-disable-next-line security/detect-unsafe-regex
   const id = value.match(/((?<id>[1-9][0-9]*)[ ]+)(disc|active)/i);
   if (id) {
     return exec(`logoff ${id.groups.id}`);
+  }
+};
+
+const execHelper = async (command, match) => {
+  try {
+    await exec(command);
+  } catch (error) {
+    if (error.message && error.message.match(match)) {
+      // Ignore selected errors
+    } else {
+      throw error;
+    }
   }
 };
 
@@ -20,17 +34,17 @@ router.get("/version", (req, res) => {
 router.post("/reset", async (req, res, next) => {
   // Kill BP process
   try {
-    await exec("taskkill /IM automate.exe /F");
+    await execHelper(
+      "taskkill /IM automate.exe /F",
+      /process "automate.exe" not found/,
+    );
   } catch (error) {
-    if (
-      error.message &&
-      error.message.match(/process "automate.exe" not found/)
-    ) {
-      // Ignore "process not running" error, as the automate.exe does not have to run at the moment
-    } else {
-      next(error);
-      return;
-    }
+    next(error);
+    return;
+  }
+
+  if (BP_API_FULL_RESET !== "true") {
+    return;
   }
 
   // Forcibly logoff all users
@@ -44,35 +58,23 @@ router.post("/reset", async (req, res, next) => {
 
   // Restart Login Agent service
   try {
-    await exec("net stop loginagent");
+    await execHelper(
+      "net stop loginagent",
+      /(The service name is invalid)|(service is not started)/,
+    );
   } catch (error) {
-    if (
-      error.message &&
-      error.message.match(
-        /(The service name is invalid)|(service is not started)/,
-      )
-    ) {
-      // Ignore error, Login Agent service does not have to be used or the service might be alerady stopped
-    } else {
-      next(error);
-      return;
-    }
+    next(error);
+    return;
   }
 
   try {
-    await exec("net start loginagent");
+    await execHelper(
+      "net start loginagent",
+      /(The service name is invalid)|(service has already been started)/,
+    );
   } catch (error) {
-    if (
-      error.message &&
-      error.message.match(
-        /(The service name is invalid)|(service has already been started)/,
-      )
-    ) {
-      // Ignore error, Login Agent service does not have to be used or the service might be alerady running
-    } else {
-      next(error);
-      return;
-    }
+    next(error);
+    return;
   }
 
   res.status(200).end();
